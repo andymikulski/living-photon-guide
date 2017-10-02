282150f27d24607feeff7a6feb5930a82fa4f366
Andy Mikulski <andy.mikulski@gmail.com>
2017-10-02 11:32:12 -0600

    Remove lib-based less, use compiled CSS

--- site/theme/template/Content/EditButton.jsx
+++ site/theme/template/Content/EditButton.jsx
@@ -4,11 +4,5 @@ import { Tooltip, Icon } from 'antd';
 const branchUrl = 'https://github.com/ant-design/ant-design/edit/master/';
 
 export default function EditButton({ title, filename }) {
-  return (
-    <Tooltip title={title}>
-      <a className="edit-button" href={`${branchUrl}${filename}`}>
-        <Icon type="edit" />
-      </a>
-    </Tooltip>
-  );
+  return null;
 }
